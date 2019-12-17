3ce85906d9565a623336c22d486dc18162ff30b8
Brian Archibald <brian.archibald@imtapps.com>
2019-12-17 14:47:20 -0600

    Hooked up api for weather and updated template

--- the_weather/weather/views.py
+++ the_weather/weather/views.py
@@ -1,4 +1,21 @@
+import requests
 from django.shortcuts import render
 
+
 def index(request):
-    return render(request, 'weather/weather.html')
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=32ebc86efd4714fb9ea0f0aa950118d0'
+    city = 'Las Vegas'
+
+    r = requests.get(url.format(city)).json()
+    print(r.text)
+
+    city_weather = {
+            'city': city,
+            'temperate': r['main']['temp'],
+            'description': r['weather'][0]['description'],
+            'icon': r['weather'][0]['icon'],
+            }
+
+    context = {'city_weather': city_weather}
+
+    return render(request, 'weather/weather.html', context)
