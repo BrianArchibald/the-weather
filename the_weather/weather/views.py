6fb344e08344952ccf3f3db4e4723b80b5513be9
Brian Archibald <brian.archibald@imtapps.com>
2019-12-21 14:00:29 -0600

    Set up model for weather

--- the_weather/weather/views.py
+++ the_weather/weather/views.py
@@ -3,15 +3,14 @@ from django.shortcuts import render
 
 
 def index(request):
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=32ebc86efd4714fb9ea0f0aa950118d0'
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=OP_KEY'
     city = 'Las Vegas'
 
     r = requests.get(url.format(city)).json()
-    print(r.text)
 
     city_weather = {
             'city': city,
-            'temperate': r['main']['temp'],
+            'temperature': r['main']['temp'],
             'description': r['weather'][0]['description'],
             'icon': r['weather'][0]['icon'],
             }
