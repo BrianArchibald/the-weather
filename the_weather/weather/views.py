3f645b26430e4c84322c72d8ed69b4836d49d122
Brian Archibald <brian.archibald@imtapps.com>
2019-12-22 21:11:10 -0600

    started django form

--- the_weather/weather/views.py
+++ the_weather/weather/views.py
@@ -1,20 +1,29 @@
 import requests
 from django.shortcuts import render
+from .models import City
 
 
 def index(request):
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=OP_KEY'
-    city = 'Las Vegas'
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=OB_KEY'
+    # city = 'Las Vegas'
 
-    r = requests.get(url.format(city)).json()
+    cities = City.objects.all()
+    weather_data = []
 
-    city_weather = {
-            'city': city,
-            'temperature': r['main']['temp'],
-            'description': r['weather'][0]['description'],
-            'icon': r['weather'][0]['icon'],
-            }
+    for city in cities:
 
-    context = {'city_weather': city_weather}
+        r = requests.get(url.format(city)).json()
+
+        city_weather = {
+                'city': city.name,
+                'temperature': r['main']['temp'],
+                'description': r['weather'][0]['description'],
+                'icon': r['weather'][0]['icon'],
+        }
+        weather_data.append(city_weather)
+
+    print('weather_dataaaaaaaaa', weather_data)
+
+    context = {'weather_data': weather_data}
 
     return render(request, 'weather/weather.html', context)
