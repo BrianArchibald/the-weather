eb1dd09fbb31195ce7933e2c9581e341f3285a1d
Brian Archibald <brian.archibald@imtapps.com>
2020-02-15 14:23:44 -0600

    Added delete and checks for correct city, working properly

--- the_weather/weather/views.py
+++ the_weather/weather/views.py
@@ -1,15 +1,38 @@
 import requests
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import City
 from .forms import CityForm
 
 
 def index(request):
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=32ebc86efd4714fb9ea0f0aa950118d0'
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=OB_KEY'
+
+    err_msg = ''
+    message = ''
+    message_class = ''
 
     if request.method == 'POST':
         form = CityForm(request.POST)
-        form.save()
+
+        if form.is_valid():
+            new_city = form.cleaned_data['name']
+            existing_city_count = City.objects.filter(name=new_city).count()
+
+            if existing_city_count == 0:
+                r = requests.get(url.format(new_city)).json()
+                if r['cod'] == 200:
+                    form.save()
+                else:
+                    err_msg = 'City does not exist'
+            else:
+                err_msg = 'City already exits in the database'
+
+        if err_msg:
+            message = err_msg
+            message_class = 'is-danger'
+        else:
+            message = 'City added successfully'
+            message_class = 'is-success'
 
     form = CityForm()
 
@@ -28,8 +51,16 @@ def index(request):
         }
         weather_data.append(city_weather)
 
-    print('weather_dataaaaaaaaa', weather_data)
-
-    context = {'weather_data': weather_data, 'form': form}
+    context = {
+            'weather_data': weather_data,
+            'form': form,
+            'message': message,
+            'message_class': message_class
+    }
 
     return render(request, 'weather/weather.html', context)
+
+
+def delete_city(request, city_name):
+    City.objects.get(name=city_name).delete()
+    return redirect('home')
